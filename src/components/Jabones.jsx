db5bb7e1d4f51fdc5016f8264be0343d7941a5f5
Gustavo Mayo <nahuelmayo@gmail.com>
2022-09-17 20:45:56 -0300

    Img3

--- src/components/Jabones.jsx
+++ src/components/Jabones.jsx
@@ -3,7 +3,7 @@ function Jabones(){
         <div className="content_jabones">
             <div className="especialidad1">
                 <div className="info">
-                    <img id="instru1" src="./src/img/instrujabon.png" alt=""/>
+                    <img id="instru1" src={require('..img/instrujabon.png')} alt=""/>
                     <img id="instru2" src="./src/img/instrujabon.png" alt=""/>
                 </div>
             </div>
