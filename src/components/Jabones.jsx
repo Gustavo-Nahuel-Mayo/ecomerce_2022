de09fde0bb6cde188ae3162ceb95b19cbca14eae
Gustavo Mayo <nahuelmayo@gmail.com>
2022-09-17 20:53:40 -0300

    Img4

--- src/components/Jabones.jsx
+++ src/components/Jabones.jsx
@@ -1,9 +1,11 @@
+import Logo from '../img/Logo.png'
+
 function Jabones(){
     return(
         <div className="content_jabones">
             <div className="especialidad1">
                 <div className="info">
-                    <img id="instru1" src={require('..img/instrujabon.png')} alt=""/>
+                    <img id="instru1" src={Logo} alt=""/>
                     <img id="instru2" src="./src/img/instrujabon.png" alt=""/>
                 </div>
             </div>
