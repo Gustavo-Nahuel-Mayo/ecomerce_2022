e89ddfe5d4ddf95d0514676d04363736ccff24e6
Gustavo Mayo <nahuelmayo@gmail.com>
2022-09-17 20:25:07 -0300

    Img

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './Css/index.css'
-import './Css/footer.css'
-import './Css/cards.css'
-import './Css/infojabones.css'
-import './Css/inicio.css'
+import './Css/index.css';
+import './Css/footer.css';
+import './Css/cards.css';
+import './Css/infojabones.css';
+import './Css/inicio.css';
+import './img/instrujabon.png';
+import './img/Logo.png';
+import './img/Onlylogo.png';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <App />
