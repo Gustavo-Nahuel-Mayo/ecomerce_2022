f2c83cf28e828ff0a78aa2ced60ff3d426a959ed
Gustavo Mayo <nahuelmayo@gmail.com>
2022-09-18 21:24:01 -0300

    Descripciones e imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ import './Css/separation.css';
 import './img/instrujabon.png';
 import './img/Logo.png';
 import './img/Onlylogo.png';
+import Miel from './img/Miel.png';
+import Carbon from './img/Carbon.png';
+import Cacao from './img/Cacao.png';
 
 
 
@@ -23,25 +26,25 @@ function App() {
       
       <div className="wrapper">
           <Card
-              img="https://arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/PJFAUAH7WBG5HMSYVUJTQHPO3M.jpg"
-              title="Humectante Naturaleza"
-              description="Pieles secas"
+              img={Miel}
+              title="Avena, Miel y Karité"
+              description="Humectante"
               price="600.00"
               info="#instru1" 
               pago="https://www.google.com.ar/"/>
     
           
           <Card
-              img="https://content21.sabervivirtv.com/medio/2022/03/08/avena-guia-alimentos_c8b225af_1280x720.jpg"
-              title="Exfoliante de Avena"
-              description="Pieles grasas"
+              img={Carbon}
+              title="Carbón, Arcilla y Karité"
+              description="Exfoliante"
               info="#instru2"
               price="45.00"/>
 
           <Card
-              img="https://www.caracteristicas.co/wp-content/uploads/2017/05/azufre-8-e1569860188258.jpg"
-              title="Jabon de Azufre"
-              description="Psoriasis"
+              img={Cacao}
+              title="Caléndula, Miel y Cacao"
+              description="Humectante y cicatrizante"
               info="#instru3"
               price="45.00"/>
       </div>
@@ -69,13 +72,13 @@ function App() {
 
 window.onscroll = function() {
   var posicion = window.pageYOffset || document.documentElement.scrollTop;
+  var rotacion = window.pageYOffset || document.documentElement.scrollWidth;
+
 
   var elemento1 = document.getElementById("navbarimg");
-  
   var elemento3 = document.getElementById("cardid");
   
   elemento1.style.bottom = posicion * 0.3 + "px";
-  
   elemento3.style.bottom = posicion * 0.3 + "px";
 
 }
--- src/components/Jabones.jsx
+++ src/components/Jabones.jsx
@@ -1,5 +1,6 @@
 import Instru1 from '../img/instrujabon.png';
-import Instru2 from '../img/instrujabon.png';
+import Instru2 from '../img/instrujabon2.png';
+import Instru3 from '../img/instrujabon3.png';
 
 function Jabones(){
     return(
@@ -8,6 +9,7 @@ function Jabones(){
                 <div className="info">
                     <img id="instru1" src={Instru1} alt=""/>
                     <img id="instru2" src={Instru2} alt=""/>
+                    <img id="instru3" src={Instru3} alt=""/>
                 </div>
             </div>
         </div>
